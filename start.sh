f733aef8e92ab6897c2732f0c4a80bccdfd3b4e0
GardianSecure <gardiansecure@gmail.com>
2025-12-08 12:23:37 +0530

    Add confirmation message for ZAP daemon launch

--- start.sh
+++ start.sh
@@ -2,8 +2,12 @@
 # Start ZAP in daemon mode
 zap.sh -daemon -port 8080 -host 0.0.0.0
 
+#Check if it even starts
+echo "ZAP daemon launched"
+
 # Wait a bit for ZAP to initialize
 sleep 15
 
 # Start your backend
 node server.js
+
