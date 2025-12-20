cfcdb6a663041bd1444f6427c81a9c6550fd2632
GardianSecure <gardiansecure@gmail.com>
2025-12-20 15:00:40 +0530

    Fix shebang and formatting in start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,11 @@
 set -e
 
 echo "🚀 Launching ZAP daemon..."
-$ZAP_HOME/zap.sh -daemon -port 8080 -host 0.0.0.0 -config api.disablekey=true &
+zap.sh -daemon -port 8080 -host 0.0.0.0 -config api.disablekey=true &
 
 # Give ZAP enough time to boot
 sleep 45
 
 echo "🚀 Launching backend..."
 node server.js
+
